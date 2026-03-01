4c8bffe8dd0184a44802081729bf589510b2978b
Abdulkarim Anzuluni <aban0001@stud.ek.dk>
2026-03-01 20:46:38 +0100

    small js issues fixed

--- js-mappe/productlist.js
+++ js-mappe/productlist.js
@@ -8,21 +8,18 @@ fetch("https://kea-alt-del.dk/t7/api/products")
     showProducts(data);
   });
 
-function showProduct(producttsArr) {
-  //   console.log("productArr", productsArr);
-  productArr.forEach((product) => {
-    console.log("product", product.id);
+function showProducts(productsArr) {
+  productContainer.innerHTML = "";
 
-    productContainer.innerHTML += `<article class="smallProduct">
-        <img src="https://kea-alt-del.dk/t7/images/webp/640/1163.webp" alt="product image" />
-        <h3>Sahara Team India Fanwear Round Neck Jersey</h3>
-        <p class="subtle">Tshirts | Nike</p>
-        <p class="price">DKK <span>1595</span>,-</p>
-        <div class="discounted">
-          <p>Now DKK <span></span>,-</p>
-          <p><span></span>%</p>
-        </div>
-        <a href="product.html">Read More</a>
-      </article>`;
+  productsArr.forEach((product) => {
+    productContainer.innerHTML += `
+      <article class="smallProduct">
+        <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="product image" />
+        <h3>${product.productdisplayname}</h3>
+        <p class="subtle">${product.articletype} | ${product.brandname}</p>
+        <p class="price">DKK <span>${product.price}</span>,-</p>
+        <a href="product.html?id=${product.id}">Read More</a>
+      </article>
+    `;
   });
 }
